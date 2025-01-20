f518be3b34876bc29a0a133b140a91194955c9db
gts.senna <gts.senna@gmail.com>
2025-01-20 15:04:25 -0300

    first project structure

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Exemplo: React.FC = () => {
+  return (
+    <nav className="text-orange-500">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+    </nav>
+  )
+};
+
+export default Exemplo;
