489725b31745b0b810ce047c48176a906687bf7a
gts.senna <gts.senna@gmail.com>
2025-01-25 00:09:24 -0300

    Revert "FEAT: add componente de exibição de fotos"
    This reverts commit ee23a4816ffd62ca3311f65c7e89a27d38d180e0.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ExbihitionsSection from "./components/ExbihitionsSection";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import NegotiateSection from "./components/NegotiateSection";
@@ -11,7 +10,6 @@ export default function Home() {
       <Header />
       <Intro />
       <NegotiateSection />
-      <ExbihitionsSection />
       <footer className="row-start-3 flex gap-6 flex-wrap items-start justify-center"></footer>
     </div>
   );
